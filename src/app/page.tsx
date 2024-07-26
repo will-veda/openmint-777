ef25c01798af3cd426cd1e1733670ea1519734a5
lamachina <lucasmaillard@protonmail.com>
2024-07-26 16:50:40 +0200

    first draft for metadata container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 import { LightMode } from "@/components/LightMode";
 import { Button } from "@/components/ui/button";
-import { useCallback, useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import AtomicalCard from "@/components/MetaData";
+import { fetchAtomicalsData } from "@/utils/fetchAtomicalData";
+import { fetchAdditionalData } from "@/utils/imageUtils";
 
 const ConnectButton = (props: any) => {
   return <Button {...props} />;
 };
 
+
+
 export default function Home() {
   const [connectedAddress, setConnectedAddress] = useState<string | null>(null);
+  const [atomicalImageData, setAtomicalImageData] = useState<any>(null);
+  const [additionalData, setAdditionalData] = useState<any>(null);
 
   const handleConnectWallet = async () => {
     if (typeof window.wizz !== "undefined") {
@@ -19,6 +26,15 @@ export default function Home() {
         const connectedAddress = accounts[0];
         setConnectedAddress(connectedAddress);
         console.log("connect success", connectedAddress);
+
+        // Fetch Atomicals data
+        const data = await fetchAtomicalsData('0420061a2d324d59df226281a33e973cca4e3a652ef2f49c360e842e4557f886i0');
+        const image = await fetchAdditionalData(data);
+
+        console.log("Atomicals data: ", data);
+
+        setAtomicalImageData(data);
+        setAdditionalData(image);
       } catch (e) {
         console.log("connect failed");
         setConnectedAddress(null);
@@ -30,11 +46,11 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-start h-full gap-8 pt-20">
       <div className="absolute top-4 right-4">
         <LightMode />
         <ConnectButton className="mx-4" onClick={handleConnectWallet}>
-        {connectedAddress
+          {connectedAddress
             ? `${connectedAddress.slice(0, 6)}...${connectedAddress.slice(-4)}`
             : "Connect"}
         </ConnectButton>
@@ -44,6 +60,8 @@ export default function Home() {
         OPEN
         <span className="text-secondary-foreground">MINT</span>
       </h1>
+
+      {atomicalImageData && <AtomicalCard atomicalData={atomicalImageData} additionalData={additionalData} />}
     </div>
   );
 }
